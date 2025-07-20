28ef2c64a2c145ead85f076a9d452b07630a0821
h-shimamon <bangdoll2000jp@yahoo.co.jp>
2025-07-20 17:43:26 +0900

    社員登録画面追加

--- app/company/forms.py
+++ app/company/forms.py
@@ -3,6 +3,7 @@
 from flask_wtf import FlaskForm
 from wtforms import StringField, DateField, IntegerField, SubmitField
 from wtforms.validators import DataRequired, Optional
+from wtforms.widgets import TextInput
 
 class EmployeeForm(FlaskForm):
     """従業員情報を登録・編集するためのフォーム"""
@@ -11,8 +12,8 @@ class EmployeeForm(FlaskForm):
     joined_date = DateField('社員となった日', validators=[Optional()])
     relationship = StringField('続柄', validators=[Optional()])
     address = StringField('住所', validators=[Optional()])
-    shares_held = IntegerField('株式数', validators=[Optional()])
-    voting_rights = IntegerField('議決権', validators=[Optional()])
+    shares_held = IntegerField('株式数', validators=[Optional()], widget=TextInput())
+    voting_rights = IntegerField('議決権', validators=[Optional()], widget=TextInput())
     position = StringField('役職', validators=[Optional()])
-    investment_amount = IntegerField('出資金額', validators=[Optional()])
+    investment_amount = IntegerField('出資金額', validators=[Optional()], widget=TextInput())
     submit = SubmitField('登録する')
