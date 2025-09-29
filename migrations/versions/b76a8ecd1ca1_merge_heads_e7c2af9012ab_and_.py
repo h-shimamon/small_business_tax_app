8eede33e64234d73c9da719a5561b8abaac7cf9b
h-shimamon <bangdoll2000jp@yahoo.co.jp>
2025-09-29 14:03:36 +0900

    Merge Alembic heads and add python-dotenv dependency

---
+++ migrations/versions/b76a8ecd1ca1_merge_heads_e7c2af9012ab_and_.py
@@ -0,0 +1,24 @@
+"""Merge heads e7c2af9012ab and a6d2b1c34e12
+
+Revision ID: b76a8ecd1ca1
+Revises: e7c2af9012ab, a6d2b1c34e12
+Create Date: 2025-09-29 13:57:25.469990
+
+"""
+from alembic import op
+import sqlalchemy as sa
+
+
+# revision identifiers, used by Alembic.
+revision = 'b76a8ecd1ca1'
+down_revision = ('e7c2af9012ab', 'a6d2b1c34e12')
+branch_labels = None
+depends_on = None
+
+
+def upgrade():
+    pass
+
+
+def downgrade():
+    pass
